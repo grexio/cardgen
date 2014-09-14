afd9b5ed28a1ce5842ccd612b69d95725f7aeab3
Javed Khan <tuxcanfly@gmail.com>
2014-09-14 22:15:01 +0530

    show/hide

--- js/cardgen.js
+++ js/cardgen.js
@@ -1,5 +1,7 @@
 (function() {
+    $("#cardcode").hide();
     $("#cardgen").click(function () {
+        $("#cardcode").show();
         var source   = $("#card-template").html();
         var template = Handlebars.compile(source);
         var context = $("#cardform").serializeObject();
